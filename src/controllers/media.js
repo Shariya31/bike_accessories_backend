4f2550ea9be2ee52c9ee5db280901d13fef5527c
Shariya31 <abushariya31@gmail.com>
2026-04-13 00:11:54 +0530

    media delete restore

--- src/controllers/media.js
+++ src/controllers/media.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { TryCatch } from "../middlewares/error.js";
 import MediaModel from "../models/Media.js";
 import cloudinary from "../utils/cloudinary.js";
@@ -56,7 +57,7 @@ export const createMedia = TryCatch(async (req, res, next) => {
         const newMedia = await MediaModel.insertMany(newFiles);
 
         return res.status(201).json({
-           success: true,
+            success: true,
             message:
                 duplicateFiles.length > 0
                     ? `${newMedia.length} media uploaded, ${duplicateFiles.length} duplicate file(s) skipped`
@@ -84,24 +85,24 @@ export const createMedia = TryCatch(async (req, res, next) => {
     }
 });
 
-export const getMedia = TryCatch(async(req, res, next) => {
+export const getMedia = TryCatch(async (req, res, next) => {
 
     const searchParams = req.query
     const page = parseInt(searchParams.page, 10) || 0
     const limit = parseInt(searchParams.limit, 10) || 10
-    const deleteType = searchParams.deleteType 
+    const deleteType = searchParams.deleteType
     //SD - Soft Delete,  RSD - Restore Soft Delete, PD - Permanent Delete
 
     let filter = {}
-    if(deleteType === 'SD'){
-        filter = {deletedAt: null}
-    } else if(deleteType === 'PD'){
-        filter = {deletedAt: {$ne: null}}
+    if (deleteType === 'SD') {
+        filter = { deletedAt: null }
+    } else if (deleteType === 'PD') {
+        filter = { deletedAt: { $ne: null } }
     }
 
-    const mediaData = await MediaModel.find(filter).sort({createdAt: -1}).skip(page * limit).limit(limit).lean()
+    const mediaData = await MediaModel.find(filter).sort({ createdAt: -1 }).skip(page * limit).limit(limit).lean()
 
-    const totalMedia = await MediaModel.countDocuments(filter) 
+    const totalMedia = await MediaModel.countDocuments(filter)
 
     res.status(200).json({
         success: true,
@@ -110,3 +111,83 @@ export const getMedia = TryCatch(async(req, res, next) => {
         hasMore: (page + 1) * limit < totalMedia
     })
 })
+
+export const updateMediaStatus = TryCatch(async (req, res, next) => {
+
+    const { payload } = req.body;
+
+    const ids = payload?.ids || req.body.ids || [];
+    const deleteType = payload?.deleteType || req.body.deleteType;
+
+    if (!Array.isArray(ids) || ids.length === 0) {
+        return next(new ErrorHandler('Invalid Or Empty id list', 400))
+    }
+
+    const media = await MediaModel.find({ _id: { $in: ids } }).lean()
+
+    if (!media.length) {
+        return next(new ErrorHandler('No Media Found', 404))
+    }
+
+    if (!['SD', 'RSD'].includes(deleteType)) {
+        return next(new ErrorHandler('Invalid Delete Operation Deletetype should be SD or RSD', 400))
+    }
+
+    if (deleteType === 'SD') {
+        await MediaModel.updateMany({ _id: { $in: ids } }, { $set: { deletedAt: new Date().toISOString() } });
+    } else {
+        await MediaModel.updateMany({ _id: { $in: ids } }, { $set: { deletedAt: null } });
+    }
+
+    res.status(200).json({
+        success: true,
+        message: `${deleteType === 'SD' ? 'Media Moved To Trash' : 'Media Restored Successfully'}`
+    });
+});
+
+export const deleteMedia = TryCatch(async (req, res, next) => {
+
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+
+    const { payload } = req.body;
+
+    const ids = payload.ids || []
+    const deleteType = payload.deleteType
+
+    if (!Array.isArray(ids) || ids.length === 0) {
+        return next(new ErrorHandler('Invalid Or Empty id list', 400))
+    }
+
+    const media = await MediaModel.find({ _id: { $in: ids } }).session(session).lean()
+
+    if (!media.length) {
+        return next(new ErrorHandler('No Media Found', 404))
+    }
+
+    if (deleteType !== 'PD') {
+        return next(new ErrorHandler('Invalid Delete Operation Deletetype should be PD', 400))
+    }
+
+    await MediaModel.deleteMany({ _id: { $in: ids } }).session(session)
+
+    //Delete media from cloudinary
+    const publicIds = media.map(m => m.public_id)
+
+    try {
+        await cloudinary.api.delete_resources(publicIds)
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        return next(error)
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(200).json({
+        success: true,
+        message: 'Media Deleted Permanently'
+    });
+});
--- src/routes/media.js
+++ src/routes/media.js
@@ -1,12 +1,18 @@
 import express from 'express'
-import { createMedia, getMedia } from '../controllers/media.js';
+import { createMedia, deleteMedia, getMedia, updateMediaStatus } from '../controllers/media.js';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 import { isAuthorized } from '../middlewares/isAuthorized.js';
 import { ROLES } from '../constants/roles.js';
 
 const router = express();
 
-router.post('/create', isAuthenticated, isAuthorized(ROLES.ADMIN, ROLES.USER), createMedia)
-router.get('/get-media', isAuthenticated, isAuthorized(ROLES.ADMIN, ROLES.USER), getMedia)
+router.post('/create', isAuthenticated, isAuthorized(ROLES.ADMIN), createMedia);
+router.get('/get-media', isAuthenticated, isAuthorized(ROLES.ADMIN), getMedia);
+
+// To soft delete or restore the media
+router.put('/update-status', isAuthenticated, isAuthorized(ROLES.ADMIN), updateMediaStatus)
+
+//Permanently delete the media
+router.delete('/delete', isAuthenticated, isAuthorized(ROLES.ADMIN), deleteMedia)
 
 export default router;

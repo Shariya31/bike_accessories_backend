46a83c6be5d055101adfabfec21b2266419f31b1
Shariya31 <abushariya31@gmail.com>
2026-04-27 19:52:40 +0530

    category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -29,7 +29,7 @@ export const createCategory = TryCatch(async (req, res, next) => {
 
     const { name, slug } = parsed.data
 
-     const existingCategory = await CategoryModel.findOne({
+    const existingCategory = await CategoryModel.findOne({
         $or: [{ name }, { slug }]
     });
 
@@ -46,5 +46,143 @@ export const createCategory = TryCatch(async (req, res, next) => {
         success: true,
         message: 'Category added successfully',
         newCategory
+    })
+})
+
+export const getAllCategory = TryCatch(async (req, res, next) => {
+
+    const query = req.query
+    const start = parseInt(query.start || 0, 10)
+    const size = parseInt(query.size || 10, 10);
+    const filters = JSON.parse(query.filters || '[]');
+    const globalFilter = query.globalFilter || "";
+    const sorting = JSON.parse(query.sorting || '[]');
+    const deleteType = query.deleteType
+
+    // Build Match query
+    let matchQuery = {}
+    if (!['SD', 'PD'].includes(deleteType)) {
+    return next(new ErrorHandler('Invalid deleteType', 400))
+}
+    if (deleteType === 'SD') {
+        matchQuery = { deletedAt: null }
+    } else if (deleteType === 'PD') {
+        matchQuery = { deletedAt: { $ne: null } }
+    }
+
+    // Global Search
+    if (globalFilter) {
+        matchQuery["$or"] = [
+            { name: { $regex: globalFilter, $options: 'i', } },
+            { slug: { $regex: globalFilter, $options: 'i', } },
+        ]
+    }
+
+    // Column Filteration
+
+    filters.forEach(filter => {
+        matchQuery[filter.id] = { $regex: filter.value, $options: 'i' }
+    })
+
+    //Sorting
+
+    let sortQuery = {}
+    sorting.forEach(sort => {
+        sortQuery[sort.id] = sort.desc ? -1 : 1
+    });
+
+    // Aggregation pipeline
+
+    const aggregatePipeline = [
+        { $match: matchQuery },
+        { $sort: Object.keys(sortQuery).length ? sortQuery : { createdAt: -1 } },
+        { $skip: start },
+        { $limit: size },
+        {
+            $project: {
+                _id: 1,
+                name: 1,
+                slug: 1,
+                createdAt: 1,
+                updatedAt: 1,
+                deletedAt: 1
+            }
+        }
+    ]
+
+    // Execute query
+
+    const getCategory = await CategoryModel.aggregate(aggregatePipeline)
+
+    // Get total row count
+    const totalRowCount = await CategoryModel.countDocuments(matchQuery)
+    res.status(200).json({
+        success: true,
+        message: 'Category fetched successfully',
+        data: getCategory,
+        meta: { totalRowCount }
+    })
+})
+
+export const updateCategoryStatus = TryCatch(async (req, res, next) => {
+
+    const { payload } = req.body;
+
+    const ids = payload?.ids || req.body.ids || [];
+    const deleteType = payload?.deleteType || req.body.deleteType;
+
+    if (!Array.isArray(ids) || ids.length === 0) {
+        return next(new ErrorHandler('Invalid Or Empty id list', 400))
+    }
+
+    const category = await CategoryModel.find({ _id: { $in: ids } }).lean()
+
+    if (!category.length) {
+        return next(new ErrorHandler('No category Found', 404))
+    }
+
+    if (!['SD', 'RSD'].includes(deleteType)) {
+        return next(new ErrorHandler('Invalid Delete Operation Deletetype should be SD or RSD', 400))
+    }
+
+    if (deleteType === 'SD') {
+        await CategoryModel.updateMany({ _id: { $in: ids } }, { $set: { deletedAt: new Date().toISOString() } });
+    } else {
+        await CategoryModel.updateMany({ _id: { $in: ids } }, { $set: { deletedAt: null } });
+    }
+
+    res.status(200).json({
+        success: true,
+        message: 'Category status updated successfully'
+    })
+})
+
+export const deleteCategory = TryCatch(async (req, res, next) => {
+
+    const { payload } = req.body;
+
+    const ids = payload.ids || []
+    const deleteType = payload.deleteType
+
+    if (!Array.isArray(ids) || ids.length === 0) {
+        return next(new ErrorHandler('Invalid Or Empty id list', 400))
+    }
+
+    const category = await CategoryModel.find({ _id: { $in: ids } }).lean()
+
+    if (!category.length) {
+        return next(new ErrorHandler('No category Found', 404))
+    }
+
+    if (deleteType !== 'PD') {
+        return next(new ErrorHandler('Invalid Delete Operation Deletetype should be PD', 400))
+    }
+
+    await CategoryModel.deleteMany({ _id: { $in: ids } })
+
+
+    res.status(200).json({
+        success: true,
+        message: 'Category deleted successfully'
     })
 })
--- src/routes/category.js
+++ src/routes/category.js
@@ -2,10 +2,13 @@ import express from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 import { isAuthorized } from '../middlewares/isAuthorized.js';
 import { ROLES } from '../constants/roles.js';
-import { createCategory } from '../controllers/category.js';
+import { createCategory, deleteCategory, getAllCategory, updateCategoryStatus } from '../controllers/category.js';
 
 const router =  express();
 
-router.post('/create', isAuthenticated, isAuthorized(ROLES.ADMIN), createCategory)
+router.get('/', isAuthenticated, isAuthorized(ROLES.ADMIN), getAllCategory);
+router.post('/create', isAuthenticated, isAuthorized(ROLES.ADMIN), createCategory);
+router.put('/update-status', isAuthenticated, isAuthorized(ROLES.ADMIN), updateCategoryStatus);
+router.delete('/delete', isAuthenticated, isAuthorized(ROLES.ADMIN), deleteCategory)
 
 export default router
